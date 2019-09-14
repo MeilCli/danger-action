a7959e13ab51935076c99e122ec401c5f10e9c49
meil <ics.sora.meil@gmail.com>
2019-09-15 06:08:47 +0900

    ignore ruby warning

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,11 @@ async function installDanger(option: Option) {
     }
 }
 
+// ignore:  {RubyPath}/gems/2.6.0/gems/git-1.5.0/lib/git/lib.rb:1029: warning: Insecure world writable dir {RubyBinPath} in PATH, mode 040777
+async function ignoreRubyWarning() {
+    await core.exportVariable("RUBYOPT", "-W0");
+}
+
 async function runDanger(option: Option) {
     await exec.exec(
         `danger --dangerfile=${option.dangerFile} --danger_id=${option.dangerId} --silent=true`,
@@ -51,6 +56,7 @@ async function run() {
         await checkEnvironment();
         const option = await getOption();
         await installDanger(option);
+        await ignoreRubyWarning();
         await runDanger(option);
     } catch (error) {
         core.setFailed(error.message);
